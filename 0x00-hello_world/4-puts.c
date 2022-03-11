01857911e7659ef586432d60a9e800fc77bc5471
Eke John <stephenjohn5646@gmail.com>
2022-03-10 23:50:33 -0800

    lets print a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - main block
- *
- *   * Return: 0
- *
- *    */
+ * main - prints a string
+ * Return : 0 if exited properly
+ */
 
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
