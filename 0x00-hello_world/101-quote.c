20124de604dac4b6276e3d5d4b192ffd2cea3c71
Eke John <stephenjohn5646@gmail.com>
2022-03-11 00:59:12 -0800

    printing a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 /**
  * main - Prints out the last part of a quote in the standard error
  * Return: 1 if success.
@@ -9,6 +7,6 @@
 
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
